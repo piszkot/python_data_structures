e62c23733162f01614424a9d4f307827502940a6
Piszkot <piszkot@piszkot.pl>
2026-03-25 08:15:36 +0100

    add array_2d.py

---
+++ array_2d.py
@@ -0,0 +1,15 @@
+# A 2D array is a collection of elements arranged in rows and columns, forming a grid-like structure. It can be thought of as a list of lists (or in Python, a tuple of tuples), where each inner sequence represents a single row of data.
+
+num_pad = ((1, 2, 3),
+           (4, 5, 6),
+           (7, 8, 9),
+           ("*", 0, "#"))
+
+print()
+for row in num_pad:
+    for num in row:
+        print(num, end=" ")
+    print()
+
+print("\n-----\n")
+print("num_pad[1][2]: ", num_pad[1][2], "\n")
